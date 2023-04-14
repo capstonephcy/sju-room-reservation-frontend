7a594ef59ad2420c8183518567cd0102794801a8
hanbikan <fchopinof99@gmail.com>
2023-04-14 15:30:38 +0900

    Add Home screen, greetings

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,6 +5,12 @@ function Home() {
     return (
       <div className='home'>
         <Navigation />
+        <div className='home-contents'>
+          <div className='home-greetings-box'>
+            <a className='home-greetings-emoji'>🙌</a>
+            <a className='home-greetings-text semi-bold'>강한빛님,<br/>회의실 예약 시스템에 오신 것을 환영합니다!</a>
+          </div>
+        </div>
       </div>
     );
 }

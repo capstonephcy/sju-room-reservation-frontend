a9f995d3b2eed79c060fcf582dc45192927df638
hanbikan <fchopinof99@gmail.com>
2023-05-08 18:43:39 +0900

    Implement toggle home and mypage in Navigation

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,18 +1,30 @@
 import './Navigation.css';
+import { useNavigate } from "react-router-dom";
 
 function Navigation() {
-    return (
-      <div className='navigation'>
-        <img className='logo-img cursor-pointer' src="/img/sejong.png" />
-        <div className='menu-box'>
-            <div className='menu-item-box background-red cursor-pointer'>
-                <img className='menu-item-logo-img' src="/img/home.png" />
-                <a className='menu-item-text semi-bold'>홈</a>
-            </div>
-        </div>
-        <img className='mypage-img cursor-pointer' src="/img/mypage.png" />
+  const navigate = useNavigate();
+
+  const toggleHome = () => {
+    navigate("/");
+  }
+
+  const toggleMyPage = () => {
+    navigate("/mypage")
+  }
+
+
+  return (
+    <div className='navigation'>
+      <img className='logo-img cursor-pointer' src="/img/sejong.png" />
+      <div className='menu-box'>
+          <div className='menu-item-box background-red cursor-pointer' onClick={toggleHome}>
+              <img className='menu-item-logo-img' src="/img/home.png" />
+              <a className='menu-item-text semi-bold'>홈</a>
+          </div>
       </div>
-    );
+      <img className='mypage-img cursor-pointer' src="/img/mypage.png" onClick={toggleMyPage} />
+    </div>
+  );
 }
 
 export default Navigation;

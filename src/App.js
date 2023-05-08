c7e1613807c1caeea0a700a4cb4097e6fc36d9d7
hanbikan <fchopinof99@gmail.com>
2023-05-08 16:05:08 +0900

    Implement different fontSize by isMobile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
 import Login from './component/Login';
+import { isMobile } from 'react-device-detect';
 
 function App() {
+  const rootFontSize = isMobile ? 12 : 16;
+  document.documentElement.style.fontSize = `${rootFontSize}px`;
   return (
     <div className="App">
       <BrowserRouter>

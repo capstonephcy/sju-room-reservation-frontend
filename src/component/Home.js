148277db1298f94138943f1018903cb0e5d075db
hanbikan <fchopinof99@gmail.com>
2023-05-08 17:46:11 +0900

    Implement wip box for mobile

--- src/component/Home.js
+++ src/component/Home.js
@@ -103,13 +103,13 @@ function Home() {
         <div className={`${mobilable('contents-with-title-box')} margin-top-2rem`}>
           <a className='contents-box-title-text'>현재 진행 중인 회의</a>
           <div className={`${mobilable('contents-box')} ${mobilable('wip-box')} margin-top-1rem`}>
-            <a className='wip-time-text'>10:00 ~ 11:00</a>
-            <a className='wip-description-text'>
+            <a className={mobilable('wip-time-text')}>10:00 ~ 11:00</a>
+            <a className={mobilable('wip-description-text')}>
               <b>836 회의실</b>에서 <b>1시간</b> 동안 진행
             </a>
-            <div className='row-box'>
-              <a className='wip-participants-text text-ellipsis'>참여자: 최지웅, 강한빛</a>
-              <div className='red-box admission-confirm-button'>
+            <div className={mobilable('wip-end-box')}>
+              <a className={`${mobilable('wip-participants-text')} text-ellipsis`}>참여자: 최지웅, 강한빛</a>
+              <div className={`red-box ${mobilable('admission-confirm-button')}`}>
                 <a className='red-box-text'>입실 인증</a>
               </div>
             </div>

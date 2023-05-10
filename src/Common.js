dc2ec4454144ff3f48fb8d89bc484575ad6adb2d
hanbikan <fchopinof99@gmail.com>
2023-05-10 18:55:22 +0900

    Fix bug where onFailure is not called for network failure

--- src/Common.js
+++ src/Common.js
@@ -8,20 +8,24 @@ export const mobilable = (str) => {
 }
 
 export const fetchUserProfile = async (onSuccess, onFailure) => {
-    const token = localStorage.getItem('refreshToken');
-    const response = await fetch(BASE_URL + '/users/profiles', {
-      method: 'GET',
-      headers: {
-        'Request-Type': 'CURRENT',
-        'Authorization': `Bearer ${token}`
-      }
-    });
+    try {
+      const token = localStorage.getItem('refreshToken');
+      const response = await fetch(BASE_URL + '/users/profiles', {
+        method: 'GET',
+        headers: {
+          'Request-Type': 'CURRENT',
+          'Authorization': `Bearer ${token}`
+        }
+      });
 
-    const data = await response.json();
-    const statusCode = response.status;
-    if (statusCode == 200) {
-      onSuccess(data);
-    } else {
-      onFailure(data);
+      const data = await response.json();
+      const statusCode = response.status;
+      if (statusCode == 200) {
+        onSuccess(data);
+      } else {
+        onFailure();
+      }
+    } catch (error) {
+      onFailure();
     }
   }

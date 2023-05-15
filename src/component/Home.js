ad2e2c019538cee7469351ca51de30524805ed05
hanbikan <fchopinof99@gmail.com>
2023-05-15 23:32:56 +0900

    Clean code

--- src/component/Home.js
+++ src/component/Home.js
@@ -9,6 +9,7 @@ import RoomModal from './RoomModal';
 import RepModal from './RepModal';
 import ReservationBox from './ReservationBox';
 import RoomListBox, { fetchRoom } from './RoomListBox';
+import OngoingMeeting from './OngoingMeeting';
 
 function Home() {
   const navigate = useNavigate();
@@ -37,34 +38,12 @@ function Home() {
         </div>
 
         <ReservationBox rooms={rooms} setShowsRepModal={setShowsRepModal} />
-
-        <div className={`${mobilable('contents-with-title-box')} margin-top-2rem`}>
-          <a className='contents-box-title-text'>현재 진행 중인 회의</a>
-          <div className={`${mobilable('contents-box')} ${mobilable('wip-box')} margin-top-1rem`}>
-            <a className={mobilable('wip-time-text')}>10:00 ~ 11:00</a>
-            <a className={mobilable('wip-description-text')}>
-              <b>836 회의실</b>에서 <b>1시간</b> 동안 진행
-            </a>
-            <div className={mobilable('wip-end-box')}>
-              <a className={`${mobilable('wip-participants-text')} text-ellipsis`}>참여자: 최지웅, 강한빛</a>
-              <div className={`red-box ${mobilable('admission-confirm-button')}`}>
-                <a className='red-box-text'>입실 인증</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <OngoingMeeting />
         <RoomListBox rooms={rooms} showingRoom={showingRoom} setShowingRoom={setShowingRoom} />
       </div>
 
-
-      {showsRepModal &&
-        <RepModal closeModal={() => { setShowsRepModal(false); }} />
-      }
-
-      {showingRoom != null &&
-        <RoomModal room={showingRoom} closeModal={() => { setShowingRoom(null); }} />
-      }
+      {showsRepModal && <RepModal closeModal={() => { setShowsRepModal(false); }} />}
+      {showingRoom != null && <RoomModal room={showingRoom} closeModal={() => { setShowingRoom(null); }} />}
     </div>
   );
 }

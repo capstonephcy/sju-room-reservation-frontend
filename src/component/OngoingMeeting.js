c6e8b66616e1b494299b4daf73dd9c7ca5f5d47b
hanbikan <fchopinof99@gmail.com>
2023-05-16 18:53:20 +0900

    Implement toggleCheckIn

--- src/component/OngoingMeeting.js
+++ src/component/OngoingMeeting.js
@@ -1,7 +1,33 @@
-import { mobilable } from "../Common";
+import { BASE_URL, mobilable } from "../Common";
 import './OngoingMeeting.css';
 
 function OngoingMeeting() {
+    const toggleCheckIn = async () => {
+        const checkInCode = prompt('회의실 내에 있는 인증 코드를 입력하세요', '');
+        try {
+            const response = await fetch(BASE_URL + '/reservation/profiles/checkin', {
+                method: 'PUT',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: 1, // TODO: room's id
+                    checkInCode: checkInCode
+                })
+            });
+    
+            const data = await response.json();
+            const statusCode = response.status;
+            if (statusCode == 200) {
+                alert("인증에 성공했습니다.");
+            } else {
+                alert("인증에 실패했습니다.");
+            }
+        } catch (error) {
+            alert("인증에 실패했습니다.");
+        }
+    }
+
     return (
         <div className={`${mobilable('contents-with-title-box')} margin-top-2rem`}>
             <a className='contents-box-title-text'>현재 진행 중인 회의</a>
@@ -13,7 +39,7 @@ function OngoingMeeting() {
                 <div className={mobilable('ongoing-meeting-end-box')}>
                     <a className={`${mobilable('ongoing-meeting-participants-text')} text-ellipsis`}>참여자: 최지웅, 강한빛</a>
                     <div className={`red-box ${mobilable('admission-confirm-button')}`}>
-                    <a className='red-box-text'>입실 인증</a>
+                    <a className='red-box-text' onClick={toggleCheckIn}>입실 인증</a>
                     </div>
                 </div>
             </div>

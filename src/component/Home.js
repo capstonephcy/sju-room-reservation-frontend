5b647f0ca4590b47d809a7ae1604d45c1b43dcd5
hanbikan <fchopinof99@gmail.com>
2023-05-19 15:26:27 +0900

    Implement checking admin for Home route to redirect to static page

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,16 +2,23 @@ import './Home.css';
 import Navigation from './Navigation';
 import 'react-calendar/dist/Calendar.css';
 import { useEffect, useState } from 'react';
-import { mobilable } from '../Common';
+import { checkIsAdmin, mobilable } from '../Common';
 import ReservationBox from './ReservationBox';
 import RoomListBox, { fetchRoom } from './RoomListBox';
 import HomeGreetings from './HomeGreetings';
 import OngoingReservation from './OngoingReservation';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+
   const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
+    if (checkIsAdmin) {
+      alert("관리자 계정은 접근할 수 없는 페이지입니다.");
+      navigate("/statics");
+    }
     fetchRoom(setRooms);
   }, []);
 

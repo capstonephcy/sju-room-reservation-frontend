585119ef77c3dff03b0a92733318f0b4bc5e434f
Kevin Park <krapi0314@gmail.com>
2023-05-23 23:08:59 +0900

    Test notifications

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,19 +8,49 @@ import RoomListBox, { fetchRoom } from './RoomListBox';
 import HomeGreetings from './HomeGreetings';
 import OngoingReservation from './OngoingReservation';
 import { useNavigate } from 'react-router-dom';
+import { getFirebaseToken, onMessageListener } from './FirebaseConfig';
 
 function Home() {
   const navigate = useNavigate();
 
   const [rooms, setRooms] = useState([]);
-
+  const [isTokenFound, setTokenFound] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+  
   useEffect(() => {
     if (checkIsAdmin()) {
       alert("관리자 계정은 접근할 수 없는 페이지입니다.");
       navigate("/statics");
     }
     fetchRoom(setRooms);
+    getFirebaseToken(setTokenFound);
   }, []);
+
+  onMessageListener()
+    .then((payload) => {
+      console.log(payload);
+
+      const title = payload.notification.title;
+      const body = payload.notification.body;
+      const message = {
+        id: 1,
+        title: title,
+        subText: body,
+        time: '1 min ago',
+        icon: 'mdi mdi-comment-account-outline',
+        variant: 'primary',
+        isRead: true,
+      };
+
+      const messages = [];
+      messages.push(message);
+
+      const notification = { day: 'Today', messages: messages };
+      notifications.push(notification);
+
+      setNotifications(notifications);
+    })
+    .catch((err) => console.log('failed: ', err));
 
   return (
     <div className={mobilable('home')}>

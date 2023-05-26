d1f5f3bd34456a45e3c3a1f82c93ae7cac5e9d26
hanbikan <fchopinof99@gmail.com>
2023-05-26 17:17:54 +0900

    Modify BASE_URL

--- src/Common.js
+++ src/Common.js
@@ -1,6 +1,6 @@
 import { isMobile } from 'react-device-detect';
 
-export const BASE_URL = "http://127.0.0.1:8080";
+export const BASE_URL = "http://211.180.114.56";
 
 export const mobilable = (str) => {
     if (isMobile) return str + '-mobile'
